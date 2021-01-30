48e8c3237b23cbf43f3da5e0d072ff8dc0560db7
Jimmy Jin <jimmy@stripe.com>
2021-01-30 11:04:19 -0800

    complete day1

--- day1/day1.go
+++ day1/day1.go
@@ -13,5 +13,38 @@ func main() {
 	data, err := ioutil.ReadFile("input.txt")
 	check(err)
 
-	fmt.Print(string(data))
+    input := string(data)
+    fmt.Println(part1(input))
+    fmt.Println(part2(input))
+}
+
+func part1(input string) int {
+    floor := 0
+    
+    for _, c := range input {
+        if c == '(' {
+            floor += 1
+        } else if c == ')' {
+            floor -= 1
+        }
+    }
+    return floor
+}
+
+func part2(input string) int {
+    floor := 0
+
+    for i, c := range input {
+        if c == '(' {
+            floor += 1
+        } else if c == ')' {
+            floor -= 1
+        }
+
+        if floor == -1 {
+            // indexes start at 1
+            return i+1
+        }
+    }
+    return 0
 }
