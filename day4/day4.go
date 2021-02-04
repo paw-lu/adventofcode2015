a3f6fc0fe28077976e3653e05e5b73aeda62c11b
Jimmy Jin <jimmy@stripe.com>
2021-02-03 22:28:09 -0800

    complete day4

---
+++ day4/day4.go
@@ -0,0 +1,50 @@
+package main
+
+import "crypto/md5"
+import "encoding/hex"
+import "fmt"
+import "strconv"
+
+func main() {
+	input := "iwrupvqb"
+
+	fmt.Println(part1(input))
+	fmt.Println(part2(input))
+}
+
+func getMd5Hex(input string) string {
+	hash := md5.Sum([]byte(input))
+	return hex.EncodeToString(hash[:])
+}
+
+func part1(input string) int {
+	i := 0
+	for {
+		testString := input + strconv.Itoa(i)
+		hexString := getMd5Hex(testString)
+
+		if hexString[:5] == "00000" {
+			break
+		}
+
+		i++
+	}
+
+	return i
+}
+
+func part2(input string) int {
+	i := 0
+	for {
+		testString := input + strconv.Itoa(i)
+		hexString := getMd5Hex(testString)
+
+		if hexString[:6] == "000000" {
+			break
+		}
+
+		i++
+	}
+
+	return i
+}
