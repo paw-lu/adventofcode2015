1c5ea8fa161c38da76eca4614892a5ebd0639d90
Diana Lam <yichi.jin@gmail.com>
2021-02-08 23:37:57 -0800

    complete day6

--- day6/day6.go
+++ day6/day6.go
@@ -8,58 +8,31 @@ import "strconv"
 
 func main() {
 	fmt.Println(part1("input.txt"))
+	fmt.Println(part2("input.txt"))
 }
 
 func part1(input string) int {
 	instructions := parseInput(input)
-	g := Grid{}
+	g := BooleanGrid{}
 
 	for _, inst := range instructions {
-		g.executeInstruction(inst)
+		g.executeBooleanInstruction(inst)
 	}
-	cnt := g.countOn()
+	cnt := g.countBoolean()
 
 	return cnt
 }
 
-type Grid struct {
-	coords [1000][1000]bool
-}
+func part2(input string) int {
+	instructions := parseInput(input)
+	g := IntegerGrid{}
 
-type Instruction struct {
-	action string
-	x1     int
-	y1     int
-	x2     int
-	y2     int
-}
-
-func (g *Grid) executeInstruction(inst Instruction) {
-	for x := inst.x1; x <= inst.x2; x++ {
-		for y := inst.y1; y <= inst.y2; y++ {
-			switch inst.action {
-			case "on":
-				(&g.coords)[x][y] = true
-			case "off":
-				(&g.coords)[x][y] = false
-			case "toggle":
-				(&g.coords)[x][y] = !(&g.coords)[x][y]
-
-			}
-		}
+	for _, inst := range instructions {
+		g.executeIntegerInstruction(inst)
 	}
-}
+	cnt := g.countInteger()
 
-func (g Grid) countOn() int {
-	i := 0
-	for x := 0; x < 1000; x++ {
-		for y := 0; y < 1000; y++ {
-			if g.coords[x][y] {
-				i++
-			}
-		}
-	}
-	return i
+	return cnt
 }
 
 func parseInput(input string) []Instruction {
@@ -94,4 +67,76 @@ func parseLine(line string) Instruction {
 	} else {
 		return Instruction{"toggle", x1, y1, x2, y2}
 	}
+}
+
+type BooleanGrid struct {
+	coords [1000][1000]bool
+}
+
+type IntegerGrid struct {
+	coords [1000][1000]int
+}
+
+type Instruction struct {
+	action string
+	x1     int
+	y1     int
+	x2     int
+	y2     int
+}
+
+func (g *BooleanGrid) executeBooleanInstruction(inst Instruction) {
+	for x := inst.x1; x <= inst.x2; x++ {
+		for y := inst.y1; y <= inst.y2; y++ {
+			switch inst.action {
+			case "on":
+				(&g.coords)[x][y] = true
+			case "off":
+				(&g.coords)[x][y] = false
+			case "toggle":
+				(&g.coords)[x][y] = !(&g.coords)[x][y]
+
+			}
+		}
+	}
+}
+
+func (g *IntegerGrid) executeIntegerInstruction(inst Instruction) {
+	for x := inst.x1; x <= inst.x2; x++ {
+		for y := inst.y1; y <= inst.y2; y++ {
+			switch inst.action {
+			case "on":
+				(&g.coords)[x][y]++
+			case "off":
+				if (&g.coords)[x][y] > 0 {
+					(&g.coords)[x][y]--
+				}
+			case "toggle":
+				(&g.coords)[x][y] += 2
+
+			}
+		}
+	}
+}
+
+func (g BooleanGrid) countBoolean() int {
+	i := 0
+	for x := 0; x < 1000; x++ {
+		for y := 0; y < 1000; y++ {
+			if g.coords[x][y] {
+				i++
+			}
+		}
+	}
+	return i
+}
+
+func (g IntegerGrid) countInteger() int {
+	i := 0
+	for x := 0; x < 1000; x++ {
+		for y := 0; y < 1000; y++ {
+			i += g.coords[x][y]
+		}
+	}
+	return i
 }
