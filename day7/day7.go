173ff46eed568c0871a069af427e0dca92216335
Diana Lam <yichi.jin@gmail.com>
2021-02-13 09:54:56 -0800

    tracing logic for day7

--- day7/day7.go
+++ day7/day7.go
@@ -1,8 +1,12 @@
 package main
 
-import "bufio"
-import "fmt"
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println(part1("input.txt"))
@@ -19,66 +23,101 @@ func part1(input string) int {
 	file, _ := os.Open(input)
 	scanner := bufio.NewScanner(file)
 
-	circuit = make(map[string]Gate)
+	circuit := make(map[string]Gate)
 
 	for scanner.Scan() {
 		sink, source := parseConnection(scanner.Text())
 		circuit[sink] = source
 	}
-	result := traceCircuit(circuit)
 
-	return result["a"]
+	return traceSink("a", circuit)
 }
 
-func parseConnection(instruction string) (sink string, source Gate) {
+func parseConnection(instruction string) (string, Gate) {
 	rawSplit := strings.Split(instruction, "->")
 	sink := strings.TrimSpace(rawSplit[1])
 
 	rawSource := strings.TrimSpace(rawSplit[0])
+	source := Gate{}
 
 	if strings.Contains(rawSource, "AND") {
 		splitSource := strings.Split(rawSource, " AND ")
-		source := Gate{
+		source = Gate{
 			operator: "AND",
 			source0:  splitSource[0],
 			source1:  splitSource[1],
 		}
 	} else if strings.Contains(rawSource, "OR") {
 		splitSource := strings.Split(rawSource, " OR ")
-		source := Gate{
+		source = Gate{
 			operator: "OR",
 			source0:  splitSource[0],
 			source1:  splitSource[1],
 		}
 	} else if strings.Contains(rawSource, "RSHIFT") {
 		splitSource := strings.Split(rawSource, " RSHIFT ")
-		source := Gate{
+		shiftAmt, _ := strconv.Atoi(splitSource[1])
+
+		source = Gate{
 			operator: "RSHIFT",
 			source0:  splitSource[0],
-			param:    splitSource[1],
+			param:    shiftAmt,
 		}
 	} else if strings.Contains(rawSource, "LSHIFT") {
 		splitSource := strings.Split(rawSource, " LSHIFT ")
-		source := Gate{
-			operator: "RSHIFT",
+		shiftAmt, _ := strconv.Atoi(splitSource[1])
+
+		source = Gate{
+			operator: "LSHIFT",
 			source0:  splitSource[0],
-			param:    splitSource[1],
+			param:    shiftAmt,
 		}
 	} else if strings.Contains(rawSource, "NOT") {
-		splitSource := strings.Split(rawSource, " NOT ")
-		source := Gate{
+		splitSource := strings.Split(rawSource, "NOT ")
+		source = Gate{
 			operator: "NOT",
 			source0:  splitSource[1],
 		}
 	} else {
-		source := Gate{
-			operator: "INPUT",
-			param:    rawSource,
+		inputAmt, err := strconv.Atoi(rawSource)
+
+		if err == nil {
+			source = Gate{
+				operator: "INPUT",
+				param:    inputAmt,
+			}
+		} else {
+			source = Gate{
+				operator: "CONNECT",
+				source0:  rawSource,
+			}
 		}
 	}
 
+	return sink, source
 }
 
-func traceCircuit(circuit map[string]Gate) map[string]int {
+func traceSink(sink string, circuit map[string]Gate) int {
+	fmt.Println("tracing wire ", sink)
 
+	g := circuit[sink]
+
+	switch g.operator {
+	case "AND":
+		return traceSink(g.source0, circuit) & traceSink(g.source1, circuit)
+	case "OR":
+		return traceSink(g.source0, circuit) | traceSink(g.source1, circuit)
+	case "LSHIFT":
+		return traceSink(g.source0, circuit) << g.param
+	case "RSHIFT":
+		return traceSink(g.source0, circuit) >> g.param
+	case "NOT":
+		return ^traceSink(g.source0, circuit)
+	case "CONNECT":
+		return traceSink(g.source0, circuit)
+	case "INPUT":
+		return g.param
+	}
+
+	return 0
 }
